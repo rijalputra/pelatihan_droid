64ff89227bbdf2c6bce50d753ebe4637461af6e5
rijalputra <rijalputra1932@gmail.com>
2017-02-01 14:16:21 +0700

    Pertemuan 2

---
+++ 2_JavaClass2/app/src/androidTest/java/muhammad/rijal/falansyah/putra/a2_javaclass/ApplicationTest.java
@@ -0,0 +1,13 @@
+package muhammad.rijal.falansyah.putra.a2_javaclass;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
